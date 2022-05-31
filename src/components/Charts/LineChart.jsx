06895bd2bba513de1d7d204d289bc709e5dc6612
mehedi hasan khairul <mhkhairul78@gmail.com>
2022-05-31 17:59:14 +0600

    Earnings Details done

---
+++ src/components/Charts/LineChart.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LineChart = () => {
+  return (
+    <div>
+      LineChart
+      <h1>f</h1>
+    </div>
+  );
+};
+
+export default LineChart;
---
+++ src/components/Charts/SparkLine.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { SparklineComponent, Inject, SparklineTooltip } from "@syncfusion/ej2-react-charts";
+
+const SparkLine = ({ id, height, weight, color, data, type, currentColor }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      weight={weight}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      yName="y"
+      type={type}
+      tooltipSettings={{
+        visible: true,
+        format: "${x} : data ${y}",
+        trackLineSettings: {
+          visible: true,
+        },
+      }}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
+};
+
+export default SparkLine;
