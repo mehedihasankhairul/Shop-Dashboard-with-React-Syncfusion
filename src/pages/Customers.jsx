41b1d957d53cc2d9098cc39b1cb632c8c8ab56ad
mehedi hasan khairul <mhkhairul78@gmail.com>
2022-06-09 17:59:21 +0600

    employees page added

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Selection,
+  Filter,
+  Inject,
+  Edit,
+  Toolbar,
+  Sort,
+} from "@syncfusion/ej2-react-grids";
+
+import { customersData, customersGrid } from "../data/dummy";
+
+import { Header } from "../components";
 
 const Customers = () => {
-  return <div>Customers</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Customers" category="Page" />
+      <GridComponent
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={["Delete"]}
+        width="auto"
+        editSettings={{ allowEditing: true, allowAdding: true, allowDeleting: true }}
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Toolbar, Selection, Edit, Sort, Filter]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Customers;
